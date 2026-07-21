9eca0df9ec18571de928ea5b83fd4bc22bb8c82e
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-07-21 14:29:37 +0330

    add next-config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3.ir-tbz-sh1.arvanstorage.ir",
+        pathname: "/safiro-auth/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.safiro.ir",
+      },
+      {
+        protocol: "https",
+        hostname: "safiro.ir"
+      }
+    ],
+    // کش طولانی‌تر برای کاهش فشار روی بهینه‌ساز
+    minimumCacheTTL: 60 * 60 * 24, // ۲۴ ساعت
+  },
+  output: "standalone",
+
+  // typescript: {
+  //   ignoreBuildErrors: true // disabled type checking when run command npm run build
+  // }
 };
 
 export default nextConfig;
