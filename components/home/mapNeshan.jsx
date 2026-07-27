21f7e6fb4780ac6bb2c61ed7e40fe90e21e843a8
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2026-07-28 03:07:11 +0330

    add map

---
+++ components/home/mapNeshan.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+import nmp_mapboxgl from "@neshan-maps-platform/mapbox-gl";
+import "@neshan-maps-platform/mapbox-gl/dist/NeshanMapboxGl.css";
+import { mapKey } from "@/utils/config";
+
+export default function MapNeshan() {
+  useEffect(() => {
+    const mapOptions = {
+        mapKey: mapKey,
+        container: "map",
+        center: [51.389, 35.6892],
+        zoom: 11,
+        pitch: 0,
+        minZoom: 2,
+        maxZoom: 21,
+        trackResize: true,
+        poi: true,
+        traffic: true,
+        mapType: nmp_mapboxgl.Map.mapTypes.neshanVector,
+        mapTypeControllerOptions: {
+        show: true,
+        position: "bottom-left",
+        },
+    };
+
+    const map = new nmp_mapboxgl.Map(mapOptions);
+    return () => {
+      map.removeMapTypeControl();
+    };
+  }, []);
+
+  return (
+    <div
+      id="map"
+      className="h-125 w-full rounded-xl overflow-hidden"
+    />
+  );
+}
